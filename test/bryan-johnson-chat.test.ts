f3a4e5d46cadc47c19a9b1a4d83f79e41eda9d8e
evanfisher <evankfisher@gmail.com>
2024-08-25 17:39:31 -0700

    Initial commit

---
+++ test/bryan-johnson-chat.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BryanJohnsonChat from '../lib/bryan-johnson-chat-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bryan-johnson-chat-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BryanJohnsonChat.BryanJohnsonChatStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
